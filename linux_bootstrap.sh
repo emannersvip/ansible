c376e943e2c82b59a88ffbfa89a51b7aef09b72d
emannersvip <edson.manners@gmail.com>
2024-11-26 15:59:06 -0500

    Update linux_bootstrap.sh

--- linux_bootstrap.sh
+++ linux_bootstrap.sh
@@ -13,6 +13,7 @@ echo -e "\n--Bootstrapping User Account ${SUDO_USER:-${HOSTNAME}}..."
 # First add  USER keys.
 echo -e "\n--Check for Local user SSH directory setup--"
 if [ ! -d ${SSH_DIR} ]; then
+	echo -e "\n---- $SSH_DIR does not exist, creating it..."
 	mkdir $SSH_DIR;
 	chmod 700 $SSH_DIR
 else
@@ -20,12 +21,13 @@ else
 	if [ -f "$SSH_AUTH" ] && test "grep work $SSH_AUTH" == 0; then
 		echo '----No need to setup SSH keys'
 	else
-		echo -e "\n----Setting up SSH keys..."
+		echo -e "\n----Setting up SSH keys for ${REGULAR_USER}..."
 		touch $SSH_AUTH
  		chown ${REGULAR_USER}:${REGULAR_USER} ${SSH_AUTH}
 		cat << EOF > $SSH_AUTH
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXP7oE/7jhnxcQXNVYzTC0ZbtHV2m9sMin7rSel+byUw3jDss5FwpSkjD8/2NKxojjsONybyC0DHNB8pzhuu/oMJuwR/s48t77cW305TfR7z4uwlim1I0BlX7u8oPop1DhFG/M2H6Gequ8Wi2FtlSvmDlclUgireIpHQypgG/8AL8BxujxNZVeK0t9yHDIXESw/btii45KzqXsU3P21zGzBNB4ZR145wcL+/J/lAlRBwD5ex9B08JJvatyLFlTZXOo0gHqO25+tkVLgaWI9Ou7Q5TgrWuFPNJb+M5/kgni0YokzwZ0pG06G4Fk+d9zGT4rv/8RaxKVt3f5czkQRVIp emanners@work_ubuntu
 EOF
+		echo -e "\n----Keys added to ${SSH_AUTH}"
 	fi
 fi
 
@@ -37,7 +39,7 @@ if [ ! -d /root/.ssh ]; then
 else
  	echo -e "\n--Checking for SSH keys in /root/.ssh/authorized_keys"
 	if [ -f "/root/.ssh/authorized_keys" ] && test "grep foreman /root/.ssh/authorized_keys" == 0; then
-		echo '----No need to setup Root SSH keys'
+		echo -e '----No need to setup Root SSH keys'
 	else
 		echo -e "\n----Setting up Root SSH keys..."
 		touch /root/.ssh/authorized_keys
@@ -45,10 +47,12 @@ else
 		cat << EOF > /root/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCcGnCtPnIkI/dTA6lJHKaFskgoofFdGFbUPwMAOqp4R35BqCGtc2gcOjbCwyQjfirHzIK7r/HyKOyVp5Oz2p6bJybhaO+G2uSWvtWeXuOCs9qX4BpHt3t96eRKsqD6tN6s/fMx4knc3DbygNp14pShfevZkAhYHhoDDATvaHZuatRgJY8Oq+NumOwQcNSlbIA5jBes2lnVPAXyhai1OMShIuWpugs3Pht70G6zw43NVAqFWdSvOJRJ9QffUPuzFdvAfrai0RQuul4W9LIl0TwiUC9/tMSngCBuA8cJIBUe3WeTBaPbNAocBvTzoiFYVXpjHPJAfsCyIk9aUv2143lpv7a8euHgkdvrp5YQ8nkXzS7si7jhVs7Ycp6HW/O0npUW5s3KeE0943pclinmEwp+U+BqmX+MHl3TZCTsdhgWQFZbPNfG9aFhEIgYXjBE9kdlQQgN/dN3FDAhCswKzvFzuJg7PL00gkzqeo9CAUr/WZfM4obbTRg0pVjt9NT/9n8= foreman-proxy@katello.edsonmanners.com
 EOF
+		echo -e "\n----Keys added to /root/.ssh"
 	fi
 fi
 
-echo -e '\n--Updating apt cache and running apt upgrade'
+echo -e "\n--Updating apt cache and running apt upgrade\n"
+
 sudo apt update
 sudo apt -y upgrade
 
